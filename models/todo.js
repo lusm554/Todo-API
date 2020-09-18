9fff59c8f7096117f6da93beb030164163fac142
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-18 10:58:58 +0300

    mongodb model

---
+++ models/todo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const todoSchema = new Schema(
+    { task: String, done: String },
+    { timestamps: { createdAt: 'created_at' } }
+)
+
+const Todo = mongoose.model('todo', todoSchema)
+exports = Todo
